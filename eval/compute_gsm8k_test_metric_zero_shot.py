3373db009352f60660bd5500b656f65c7080e7b9
Ashvini Jindal <akjindal53244@gmail.com>
2023-10-17 18:27:19 -0700

    Add GSM8k Arithmo-Mistral-7B Prediction File and script for metric computation

---
+++ eval/compute_gsm8k_test_metric_zero_shot.py
@@ -0,0 +1,33 @@
+import json
+
+file_path = "data/predictions/gsm8k/Arithmo-Mistral-7B/predictions_Arithmo_gsm8k_0_shot_COT.json"
+
+def extract_ground_truth_answer(ground_truth_gen):
+    # there are cases when 250000 is written as 250,000
+    answer = ground_truth_gen.split("####")[-1].strip().replace(",", "")
+    return answer
+
+def extract_predcited_answer(predicted_gen):
+    if "The answer is:" in predicted_gen:
+        answer = predicted_gen.split("The answer is:")[-1].strip()
+        return answer
+    elif "The answer is " in predicted_gen:
+        answer = predicted_gen.split("The answer is ")[-1].strip()
+        return answer
+    else: # Answer couldn't be found in generated text. Return empty string.
+        return ""
+
+count, total = 0,0
+with open(file_path, 'r') as f:
+    data = json.load(f)
+    for d in data:
+        question = d["question"]
+        ground_truth_gen = d["ground_truth"]
+        predicted_gen = d["prediction"]
+
+        ground_truth_answer = extract_ground_truth_answer(ground_truth_gen)
+        predicted_answer = extract_predcited_answer(predicted_gen)
+        if ground_truth_answer == predicted_answer:
+            count += 1
+        total += 1
+print(f"Total Instances: {total}, Correct Count: {count}, Accuracy: {count/total}")
